45872e14233d344ce66dc52c3e86b4a784f36419
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-04-08 22:34:07 +0330

    Add Material Ui and App bar

---
+++ portfolioapp/src/Components/ui/Header.jsx
@@ -0,0 +1,14 @@
+import {AppBar, Button, Toolbar} from "@mui/material";
+
+const Header = () => {
+    return(
+        <>
+            <AppBar>
+              <Toolbar>
+                   <Button variant="secondery">کلیک کنید</Button>
+               </Toolbar>
+          </AppBar>
+        </>
+    )
+}
+export default Header;
